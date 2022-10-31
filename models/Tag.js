76d1f5b516c59b3e671ea8110ba55ac9b2bbaa0a
nhan <nhanng19@gmail.com>
2022-10-31 04:28:40 -0700

    model for tags

---
+++ models/Tag.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+const { canTreatArrayAsAnd } = require('sequelize/types/lib/utils.js');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_Name: {
+      type: DataTypes.STRING
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
