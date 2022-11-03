e4d61ee1b401817bb939b5f3f3200d35f5b5a5f5
nhan <nhanng19@gmail.com>
2022-11-02 21:11:02 -0700

    fixed routing bug

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,7 +114,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id',  async (req, res) => {
   // delete one product by its `id` value
-    try {const deleteProduct = await Product.destroy(req.body, {
+    try {const deleteProduct = await Product.destroy({
       where: {
         id: req.params.id,
       },
