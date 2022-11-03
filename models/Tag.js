c57455fb7243ec959dff402a46f871c0abfcc761
nhan <nhanng19@gmail.com>
2022-11-02 21:09:21 -0700

    fixed routing bug

--- models/Tag.js
+++ models/Tag.js
@@ -1,5 +1,4 @@
 const { Model, DataTypes } = require('sequelize');
-const { canTreatArrayAsAnd } = require('sequelize/types/lib/utils.js');
 
 const sequelize = require('../config/connection.js');
 
@@ -7,16 +6,15 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
-    tag_Name: {
-      type: DataTypes.STRING
-    }
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
